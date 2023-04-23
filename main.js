a8d839a9ea17aa6a7ace890b12a6af3632162f72
MaxiMuks <liveranr1294@gmail.com>
2023-04-23 13:53:46 +0900

    second commit

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let news = []
+// let news = []
 
-const getLatestNews = async() => {
-    let url = new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10') 
-    let header = new Headers({'x-api-key' : 'zB2nLs_Le3g1zAAaUr-Ike1cgUSkfmtnb4_2RDzYeTM'});
-    let response = await fetch(url, {headers:header});
-    let data = await response.json();
-    console.log("data", data);
-    news = data.articles
-    console.log(news);
-}
-getLatestNews();
+// const getLatestNews = async() => {
+//     let url = new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10') 
+//     let header = new Headers({'x-api-key' : 'zB2nLs_Le3g1zAAaUr-Ike1cgUSkfmtnb4_2RDzYeTM'});
+//     let response = await fetch(url, {headers:header});
+//     let data = await response.json();
+//     console.log("data", data);
+//     news = data.articles
+//     console.log(news);
+// }
+// getLatestNews();
